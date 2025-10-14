3464b54cb7019e61f1c723f052a34182b8eb65c9
AmandaEmilia <amanda.e.strom@gmail.com>
2025-10-14 08:21:13 +0200

    Initialize lobby service

---
+++ lobby/src/test/java/se/salt/lobby/LobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package se.salt.lobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
