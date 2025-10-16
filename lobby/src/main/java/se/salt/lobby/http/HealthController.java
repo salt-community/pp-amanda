47496de984e79f830e53e9fcfc1861c7d1a5b782
AmandaEmilia <amanda.e.strom@gmail.com>
2025-10-16 20:17:25 +0200

    Adding HealthController for healthcheck

---
+++ lobby/src/main/java/se/salt/lobby/http/HealthController.java
@@ -0,0 +1,22 @@
+package se.salt.lobby.http;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
+public class HealthController {
+
+    @GetMapping(value = "/health")
+    public ResponseEntity<?> health() {
+        return ResponseEntity.ok().build();
+    }
+}
