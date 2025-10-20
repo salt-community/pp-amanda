9efbd33f270ae683138c2f511b2c9543135da8e5
AmandaEmilia <amanda.e.strom@gmail.com>
2025-10-20 16:09:16 +0200

    FE flow for joining session

--- frontend/src/api/lobbyApi.ts
+++ frontend/src/api/lobbyApi.ts
@@ -1,7 +1,7 @@
 import type { SessionResponse, SessionRequest } from "../types/session";
 import { LOBBY_URL } from "../config/api";
 
-export async function createRoom(
+export async function createSession(
   sessionName: string
 ): Promise<SessionResponse> {
   const response = await fetch(`${LOBBY_URL}/create`, {
@@ -15,4 +15,17 @@ export async function createRoom(
   }
   const data = await response.json();
   return data.sessionId;
+}
+
+export async function joinSession(sessionId: string): Promise<SessionResponse> {
+  const response = await fetch(`${LOBBY_URL}/join/${sessionId}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to create room");
+  }
+
+  return await response.json();
 }
