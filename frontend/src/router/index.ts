1e132ccb0aa1ebc7ca9791ae3707ee656b14b183
AmandaEmilia <amanda.e.strom@gmail.com>
2025-10-20 16:09:46 +0200

    Prep. for building gameView

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LobbyView from "../views/LobbyView.vue";
+import GameView from "../views/GameView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", redirect: "/lobby" },
     { path: "/lobby", component: LobbyView },
+    { path: "/game", component: GameView },
   ],
 });
 
