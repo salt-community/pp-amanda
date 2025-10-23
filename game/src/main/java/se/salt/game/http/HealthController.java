b2aa141dd45093a007ac935a1fde4d416efd0cb9
AmandaEmilia <amanda.e.strom@gmail.com>
2025-10-23 18:41:34 +0200

    Add HealthController

---
+++ game/src/main/java/se/salt/game/http/HealthController.java
@@ -0,0 +1,21 @@
+package se.salt.game.http;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "")
+@AllArgsConstructor
+public class HealthController {
+
+    @GetMapping(value = "/health")
+    public ResponseEntity<?> health() {
+        return ResponseEntity.ok().build();
+    }
+}
+
