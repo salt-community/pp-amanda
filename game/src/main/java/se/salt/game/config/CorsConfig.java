6abf6a63618635de1147d0f48b505e9a40074daf
AmandaEmilia <amanda.e.strom@gmail.com>
2025-10-27 08:59:32 +0100

    Add CorsConfig

---
+++ game/src/main/java/se/salt/game/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package se.salt.game.config;
+
+import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://frontend-alb-1896419215.eu-north-1.elb.amazonaws.com",
+                        "http://frontend-alb-1896419215.eu-north-1.elb.amazonaws.com"
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
