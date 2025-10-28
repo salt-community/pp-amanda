d9da65fd7715bfa140a836ef902ad59db5e51a2e
AmandaEmilia <amanda.e.strom@gmail.com>
2025-10-28 10:29:22 +0100

    Add SessionCreatedEvent WIP integration using SQS

---
+++ lobby/src/main/java/se/salt/lobby/integration/event/SessionCreatedEvent.java
@@ -0,0 +1,12 @@
+package se.salt.lobby.integration.event;
+
+import lombok.Builder;
+
+@Builder
+public record SessionCreatedEvent(
+    String eventType,
+    String sessionId,
+    String timeStamp
+) {
+
+}
