d8086eff4771d6321643b0f8ab27e8ff4a542c4b
AmandaEmilia <amanda.e.strom@gmail.com>
2025-10-28 08:22:44 +0100

    WIP component for initialization - popup doesnt work

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,3 +1,26 @@
-<template></template>
+<template>
+  <div>
+    <InitializeGame
+      v-if="!gameType"
+      :session-id="sessionId"
+      @initialized="handleInitialized"
+    />
+    <GameBoard v-else :game-type="gameType" />
+  </div>
+</template>
 
-<script setup></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import InitializeGame from "../components/InitializeGame.vue";
+import GameBoard from "../components/GameBoard.vue";
+import type { GameType } from "../types/game";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const sessionId = route.params.sessionId as string;
+
+const gameType = ref<GameType | null>(null);
+function handleInitialized(type: GameType) {
+  gameType.value = type;
+}
+</script>
