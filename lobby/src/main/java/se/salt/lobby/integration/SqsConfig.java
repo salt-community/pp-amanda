89442ac4e9cb357f8bd5d772b31aaa96940d21b7
AmandaEmilia <amanda.e.strom@gmail.com>
2025-10-29 07:51:11 +0100

    remove endpoint for prod where it's not needed

--- lobby/src/main/java/se/salt/lobby/integration/SqsConfig.java
+++ lobby/src/main/java/se/salt/lobby/integration/SqsConfig.java
@@ -6,21 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
-import java.net.URI;
-
 @Configuration
 public class SqsConfig {
 
     @Value("${aws.region}")
     private String region;
 
-    @Value("${aws.sqs.endpoint}")
-    private String endpoint;
-
     @Bean
     public SqsClient sqsClient() {
         return SqsClient.builder()
-            .endpointOverride(URI.create(endpoint))
             .region(Region.of(region))
             .build();
     }
