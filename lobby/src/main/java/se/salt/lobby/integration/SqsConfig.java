38bf01c41a6bba4fb06ae00dac2293a9481cc0bd
AmandaEmilia <amanda.e.strom@gmail.com>
2025-10-29 12:05:58 +0100

    Take back sqsEndpoint with condition to work when using LocalStack

--- lobby/src/main/java/se/salt/lobby/integration/SqsConfig.java
+++ lobby/src/main/java/se/salt/lobby/integration/SqsConfig.java
@@ -6,16 +6,28 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
+import java.net.URI;
+
 @Configuration
 public class SqsConfig {
 
     @Value("${aws.region}")
     private String region;
 
+    @Value("${aws.sqs.endpoint:}") // tom som default i prod
+    private String sqsEndpoint;
+
     @Bean
     public SqsClient sqsClient() {
-        return SqsClient.builder()
-            .region(Region.of(region))
-            .build();
+        var builder = SqsClient.builder()
+            .region(Region.of(region));
+
+        if (sqsEndpoint != null && !sqsEndpoint.isBlank()) {
+            builder.endpointOverride(URI.create(sqsEndpoint));
+        }
+
+        return builder.build();
     }
 }
+
+
