e4b9c747fc59b41df717f4372b65750900d3fe7d
AmandaEmilia <amanda.e.strom@gmail.com>
2025-10-31 19:52:17 +0100

    Add WebSocketConfig

---
+++ game/src/main/java/se/salt/game/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package se.salt.game.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-game")
+            .setAllowedOriginPatterns(
+                "http://localhost:5173",
+                "http://frontend-alb-1896419215.eu-north-1.elb.amazonaws.com")
+            .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic"); // sub
+        registry.setApplicationDestinationPrefixes("/app"); // pub
+    }
+}
