83ff942a821356fca81b351bf8910ea139437222
AmandaEmilia <amanda.e.strom@gmail.com>
2025-11-03 21:27:28 +0100

    GameController with startGame endpoint

--- game/src/main/java/se/salt/game/http/GameController.java
+++ game/src/main/java/se/salt/game/http/GameController.java
@@ -2,6 +2,9 @@ package se.salt.game.http;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.salt.game.domain.GameService;
@@ -12,7 +15,14 @@ import se.salt.game.domain.GameService;
 @AllArgsConstructor
 public class GameController {
 
-    private final GameService gameService;
+    private final GameService service;
+
+    @PostMapping("/{gameId}/start")
+    public ResponseEntity<Void> startGame(@PathVariable String gameId) {
+        log.info("Starting game {}", gameId);
+        service.startGame(gameId);
+        return ResponseEntity.ok().build();
+    }
 
 
 }
