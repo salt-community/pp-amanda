042dd79dab5e89ec329ccad9d6cf94ec7ac49722
AmandaEmilia <amanda.e.strom@gmail.com>
2025-11-07 13:36:36 +0100

    removed endpoint, start is triggered internally

--- game/src/main/java/se/salt/game/http/GameController.java
+++ game/src/main/java/se/salt/game/http/GameController.java
@@ -2,11 +2,8 @@ package se.salt.game.http;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.salt.game.domain.GameService;
@@ -19,13 +16,6 @@ import se.salt.game.domain.model.Player;
 public class GameController {
 
     private final GameService service;
-
-    @PostMapping("/{gameId}/start")
-    public ResponseEntity<Void> startGame(@PathVariable String gameId) {
-        log.info("Starting game {}", gameId);
-        service.startGame(gameId);
-        return ResponseEntity.ok().build();
-    }
 
     @MessageMapping("/reaction")
     public void handleReaction(Player reaction, @Header("playerName") String playerName) {
