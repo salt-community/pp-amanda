37c176ff4311db774f27837767f061486c6ad8b1
AmandaEmilia <amanda.e.strom@gmail.com>
2025-11-07 14:45:45 +0100

    update condition for when to disaplay the comps.

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import CountdownBox from "../components/CountdownBox.vue";
 import GameBoard from "../components/GameBoard.vue";
@@ -8,6 +8,7 @@ import { useGameSocket } from "../composables/useGameSocket";
 const route = useRoute();
 const gameId = route.params.gameId as string;
 const playerName = route.query.player as string;
+const showCountdown = ref(true);
 
 const { connect, connected, countdownSeconds, gameOver } = useGameSocket(
   gameId,
@@ -24,9 +25,14 @@ onMounted(() => {
     <p>Status: {{ connected ? "Connected" : "Disconnected" }}</p>
     <div class="flex flex-col items-center mt-10">
       <h1 class="text-2xl font-bold mb-4">Reaction Game</h1>
-      <CountdownBox :countdown-seconds="countdownSeconds" />
+      <CountdownBox
+        v-if="showCountdown"
+        :countdown-seconds="countdownSeconds"
+        @finished="showCountdown = false"
+      />
+
       <GameBoard
-        v-if="countdownSeconds === 0 && !gameOver"
+        v-if="!showCountdown && !gameOver"
         :game-id="gameId"
         :player-name="playerName"
       />
