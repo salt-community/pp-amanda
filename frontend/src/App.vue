c2c32d090d33e7d4d2a586139bb1aec3e88d251a
AmandaEmilia <amanda.e.strom@gmail.com>
2025-11-10 10:33:27 +0100

    set global tailwind styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <RouterView />
+  <div
+    class="min-h-screen flex flex-col items-center justify-center gap-6 font-mono text-white bg-zinc-800"
+  >
+    <RouterView />
+  </div>
 </template>
 
 <script setup lang="ts"></script>
