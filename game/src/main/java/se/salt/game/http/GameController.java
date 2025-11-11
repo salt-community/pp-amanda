8ab5a61af59193ee121377b1642a4acdc287a284
AmandaEmilia <amanda.e.strom@gmail.com>
2025-11-11 09:11:25 +0100

    Add endpoint to get result for a specific game

--- game/src/main/java/se/salt/game/http/GameController.java
+++ game/src/main/java/se/salt/game/http/GameController.java
@@ -2,12 +2,18 @@ package se.salt.game.http;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.salt.game.domain.GameService;
 import se.salt.game.domain.model.Player;
+import se.salt.game.http.dto.ResultResponse;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -25,6 +31,13 @@ public class GameController {
             reaction = reaction.toBuilder().playerName(playerName).build();
         }
         service.handleReaction(reaction);
+    }
+
+    @GetMapping("/{gameId}/result")
+    public ResponseEntity<ResultResponse> gameResult(@PathVariable String gameId) {
+        Map<String, Double> scoreBoard = service.getResult(gameId);
+        log.info("Game result for game with ID {}", gameId);
+        return ResponseEntity.ok(ResultResponse.from(scoreBoard));
     }
 
 
