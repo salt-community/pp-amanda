c356ba71bee63c1e4ee038ea97553c69d37f3af5
AmandaEmilia <amanda.e.strom@gmail.com>
2025-11-11 10:31:19 +0100

    update name

--- game/src/main/java/se/salt/game/http/dto/ResultResponse.java
+++ game/src/main/java/se/salt/game/http/dto/ResultResponse.java
@@ -2,9 +2,9 @@ package se.salt.game.http.dto;
 
 import java.util.Map;
 
-public record ResultResponse(Map<String, Double> players) {
-    
-    public static ResultResponse from(Map<String, Double> players) {
-        return new ResultResponse(players);
+public record ResultResponse(Map<String, Double> results) {
+
+    public static ResultResponse from(Map<String, Double> results) {
+        return new ResultResponse(results);
     }
 }
