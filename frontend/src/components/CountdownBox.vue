1484869b15278c9b7e5fc34fb27254a40af0cf1b
AmandaEmilia <amanda.e.strom@gmail.com>
2025-11-11 15:14:04 +0100

    gpt/ai helpt with some animation for countdown

--- frontend/src/components/CountdownBox.vue
+++ frontend/src/components/CountdownBox.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="text-2xl font-mono">
-    <div v-if="secondsLeft === null">Waiting for game to start...</div>
-    <div v-else-if="secondsLeft > 0" class="quickr-title">
-      {{ secondsLeft }}...
+  <div class="text-2xl font-mono flex items-center justify-center">
+    <div v-if="secondsLeft === null" class="text-amber-600 italic">
+      Waiting for game to start...
     </div>
-    <div v-else>🎯 Go!</div>
+    <div
+      v-else-if="secondsLeft > 0"
+      class="border-8 border-double border-amber-600 rounded-xl w-28 h-28 flex items-center justify-center text-5xl font-extrabold text-amber-600 font-mono animate-[quickr-pulse_1.2s_ease-in-out_infinite]"
+    >
+      {{ secondsLeft }}
+    </div>
+    <div
+      v-else
+      class="border-8 border-double border-lime-500 rounded-xl w-32 h-32 flex items-center justify-center text-5xl font-extrabold text-lime-500 font-mono animate-[quickr-burst_0.6s_ease-out_forwards]"
+    >
+      🎯
+    </div>
   </div>
 </template>
 
