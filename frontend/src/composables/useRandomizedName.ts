de18882bc63f726eb6e2357854b1f77515303d4e
AmandaEmilia <amanda.e.strom@gmail.com>
2025-11-13 13:39:00 +0100

    add prefetch

--- frontend/src/composables/useRandomizedName.ts
+++ frontend/src/composables/useRandomizedName.ts
@@ -1,9 +1,24 @@
-import { useQuery } from "@tanstack/vue-query";
+import { useQuery, useQueryClient } from "@tanstack/vue-query";
 import { getRandomName } from "../api/gameApi";
 
 export function useRandomizedName() {
-  return useQuery({
+  const queryClient = useQueryClient();
+
+  const query = useQuery({
     queryKey: ["random-name"],
     queryFn: getRandomName,
+    enabled: false,
   });
+
+  async function prefetch() {
+    await queryClient.prefetchQuery({
+      queryKey: ["random-name"],
+      queryFn: getRandomName,
+    });
+  }
+
+  return {
+    ...query,
+    prefetch,
+  };
 }
