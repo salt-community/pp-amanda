4f6cfdd21a8e4f23ffeb873937fc4e7526f46317
AmandaEmilia <amanda.e.strom@gmail.com>
2025-11-17 14:07:13 +0100

    updated scoreboard

--- frontend/src/components/ScoreBoard.vue
+++ frontend/src/components/ScoreBoard.vue
@@ -4,19 +4,26 @@ import { useScoreBoard } from "../composables/useScoreBoard";
 const props = defineProps<{ gameId: string }>();
 
 const { data: result, isLoading, isError } = useScoreBoard(props.gameId);
+
+function getMedal(index: number) {
+  if (index === 0) return "🥇";
+  if (index === 1) return "🥈";
+  if (index === 2) return "🥉";
+  return "🐌";
+}
 </script>
 
 <template>
   <div class="text-center">
     <p v-if="isLoading">Loading...</p>
-    <p v-else-if="isError">Something went wrong 😢</p>
+    <p v-else-if="isError">Something went wrong 🥺</p>
 
-    <ul v-else>
+    <ul v-else-if="result">
       <li
         v-for="([name, time], index) in Object.entries(result.results)"
         :key="name"
       >
-        {{ index + 1 }}. {{ name }} — {{ time.toFixed(2) }}s
+        {{ getMedal(index + 1) }}. {{ name }} – {{ time.toFixed(2) }}
       </li>
     </ul>
   </div>
