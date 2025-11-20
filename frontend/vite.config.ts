a1a52d1c469d8606c65421daca779bf99c6a3378
AmandaEmilia <amanda.e.strom@gmail.com>
2025-11-20 19:31:14 +0100

    config for cleaner refactoring

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
   define: {
     global: "globalThis",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   server: {
     watch: {
