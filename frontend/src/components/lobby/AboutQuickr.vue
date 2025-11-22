9df32ed0eee6345e46855cd57c21ddf4c1f615f1
AmandaEmilia <amanda.e.strom@gmail.com>
2025-11-22 19:56:45 +0100

    add info to about page

--- frontend/src/components/lobby/AboutQuickr.vue
+++ frontend/src/components/lobby/AboutQuickr.vue
@@ -1,2 +1,21 @@
-<template><div>Here you can read about Quick-R</div></template>
-<script setup lang="ts"></script>
+<template>
+  <div class="flex flex-col items-center text-center gap-6">
+    <h2 class="text-3xl font-bold">How to play Quickr?</h2>
+
+    <p class="text-lg max-w-md">
+      It’s simple! Just tap the active cells as fast as you can 💥
+    </p>
+
+    <div class="w-20 h-1 bg-amber-600 rounded-full"></div>
+
+    <img
+      :src="gif"
+      alt="Quick-R gameplay demo"
+      class="rounded-lg w-full max-w-md bg-black p-0.5"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import gif from "@/assets/quickr.gif";
+</script>
